fadb68a83c8d76dc8f1c18322a1724015b27a0ca
Annany Vishwakarma <annany.21211@knit.ac.in>
2024-10-13 12:36:00 +0530

    resolved more deps issue

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import { connection } from "./utils/connection";
 import userRouter from "./routes/user.route";
@@ -7,6 +8,7 @@ import quizRouter from "./routes/quiz.route";
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRouter);
